4a9ca8a186fb775d7e6b87a47f3456e38681d086
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-05 16:32:57 +0300

    CSS and fat arrow

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+const Square = (props) =>
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
 
 class Matrix extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(3).fill(0)
+      squares: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ]
     };
   }
 
-  renderSquare(i) {
-    return (
+  renderSquare = (i) =>
       <Square
         value={this.state.squares[i]}
         //onClick={() => this.onClick(i)}
       />
-    );
-  }
 
-  render() {
-    return (
-      <div>
-        <div className="matrix-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-      </div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-    the hourglass
-      <div className="matrix">
-        <Matrix
-              //onClick={i => this.handleClick(i)}
-        />
+  render = () =>
+    <div>
+      <div className="matrix-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
       </div>
     </div>
-  );
 }
+
+const App = () =>
+  <div className="App">
+  the hourglass
+    <div className="matrix">
+      <Matrix
+            //onClick={i => this.handleClick(i)}
+      />
+    </div>
+  </div>
 
 export default App;
