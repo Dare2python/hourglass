f5e7281f1ad2e146718be92426a73d1f169eb189
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-05 15:10:42 +0300

    first visual

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Matrix extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(3).fill(0)
+    };
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        //onClick={() => this.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="matrix-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
     the hourglass
+      <div className="matrix">
+        <Matrix
+              //onClick={i => this.handleClick(i)}
+        />
+      </div>
     </div>
   );
 }
